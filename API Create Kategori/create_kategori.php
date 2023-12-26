9b8a534851410608bcf65253fe0f6f2564081d42
Kristinelensia <angelinasimanjuntak200403@gmail.com>
2023-12-26 12:13:22 +0700

    creat kategori

---
+++ API Create Kategori/create_kategori.php
@@ -0,0 +1,33 @@
+<?php
+$conn = mysqli_connect('localhost', 'root', '', 'apotek');
+
+$res = [
+  "status" => 200,
+  "msg" => "",
+  "body" => [
+      "data" => [
+          "nama_kategori" => "",
+      ]
+  ]
+      ];
+
+$kode = $_POST['KODE'];
+$nama = $_POST['NAMA'];
+$q = mysqli_query($conn, "INSERT INTO kategori (KODE,NAMA) VALUES ('$kode','$nama')");
+
+if ($q) {
+
+  $res['status'] = 200;
+  $res['msg'] = "Data berhasil di insert";
+  $res['body']['data']=[
+    'KODE' => $kode,
+    'NAMA' => $nama
+    ];
+} else {
+  $res['status'] = 400;
+  $res['msg'] = "Gagal membuat kategori";
+  $res['body']['error'] = "Kesalahan validasi input";
+}
+
+echo json_encode($res);
+?>

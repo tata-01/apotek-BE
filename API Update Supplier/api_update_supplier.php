ce53aac638c0cff4ac21bdd9bcba7c6247cd2e1b
Andika08Dk <andikaafrianda274@gmail.com>
2024-01-08 17:40:14 +0700

    37_api_update_supplier

--- API Update Supplier/api_update_supplier.php
+++ API Update Supplier/api_update_supplier.php
@@ -4,7 +4,9 @@ $koneksi = mysqli_connect('localhost', 'root', '', 'apotek');
 $response = [
     'status' => '',
     'msg' => '',
-    'body' => []
+    'body' => [
+        'data' => []
+    ]
 ];
 
 if (!isset($koneksi)) {
@@ -14,13 +16,17 @@ if (!isset($koneksi)) {
 } else {
     $kode = $_POST['KODE'];
     $nama = $_POST['NAMA'];
+    $alamat = $_POST['ALAMAT'];
+    $no_handphone = $_POST['NO_HANDPHONE'];
 
     $response['status'] = 200;
     $response['msg'] = 'data berhasil diubah';
     $response['body']['data']['kode'] = $kode;
     $response['body']['data']['nama'] = $nama;
+    $response['body']['data']['alamat'] = $alamat;
+    $response['body']['data']['no_handphone'] = $no_handphone;
 
-    mysqli_query($koneksi, "UPDATE kategori SET NAMA = '$nama' WHERE KODE = '$kode'");
+    mysqli_query($koneksi, "UPDATE supplier SET NAMA = '$nama', ALAMAT = '$alamat', NO_HANDPHONE = '$no_handphone' WHERE KODE = '$kode'");
 }
 
 echo json_encode($response);

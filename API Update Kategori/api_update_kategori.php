f4c6808caaf9312632301e55fba76ac579eb2fc3
Andika08Dk <andikaafrianda274@gmail.com>
2024-01-08 18:03:38 +0700

    revisi_32_api_update_kategori

---
+++ API Update Kategori/api_update_kategori.php
@@ -0,0 +1,26 @@
+<?php
+$koneksi = mysqli_connect('localhost', 'root', '', 'apotek');
+
+$response = [
+    'status' => '',
+    'msg' => '',
+    'body' => []
+];
+
+if (!isset($koneksi)) {
+
+    $response['status'] = 400;
+    $response['msg'] = 'data gagal diubah';
+} else {
+    $kode = $_POST['KODE'];
+    $nama = $_POST['NAMA'];
+
+    $response['status'] = 200;
+    $response['msg'] = 'data berhasil diubah';
+    $response['body']['data']['kode'] = $kode;
+    $response['body']['data']['nama'] = $nama;
+
+    mysqli_query($koneksi, "UPDATE kategori SET NAMA = '$nama' WHERE KODE = '$kode'");
+}
+
+echo json_encode($response);

2986484950cd3e591bba99aed2675f5a0459f879
FITRAHUL JUMA SAWIKA <129470863+Mythicomyia@users.noreply.github.com>
2024-01-09 22:40:18 +0700

    Update delete_supplier.php

--- API Delete Supplier/delete_supplier.php
+++ API Delete Supplier/delete_supplier.php
@@ -9,26 +9,26 @@ $response = [
     ]
 ];
 
-$kode = $_POST['KODE'];
-$nama = $_POST['NAMA'];
+$kode = $_POST['kode'];
+$nama = $_POST['nama'];
 $alamat = $_POST['ALAMAT'];
-$no_handphone = $_POST['NO_HANDPHONE'];
+$no_handphone = $_POST['no_telp'];
 
 
 if (!isset($koneksi)) {
 
     $response['status'] = 400;
     $response['msg'] = 'data gagal dihapus';
-    $response['body']['data']['KODE'] = $kode;
+    $response['body']['data']['kode'] = $kode;
 } else {
 
-    mysqli_query($koneksi, "DELETE FROM supplier WHERE KODE = '$kode'");
+    mysqli_query($koneksi, "DELETE FROM supplier WHERE kode = '$kode'");
     $response['status'] = 200;
     $response['msg'] = 'data berhasil dihapus';
-    $response['body']['data']['KODE'] = $kode;
-    $response['body']['data']['NAMA'] = $nama;
-    $response['body']['data']['ALAMAT'] = $alamat;
-    $response['body']['data']['NO_HANDPHONE'] = $no_handphone;
+    $response['body']['data']['kode'] = $kode;
+    $response['body']['data']['nama'] = $nama;
+    $response['body']['data']['alamat'] = $alamat;
+    $response['body']['data']['no_telp'] = $no_handphone;
 }
 
 
